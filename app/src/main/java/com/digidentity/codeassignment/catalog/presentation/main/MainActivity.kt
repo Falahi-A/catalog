200ebbbc0752cf8eaa5b4c71f48aecb569555143
Falahi-A <falahi.ali@hotmail.com>
2021-11-13 13:14:46 +0330

    packaging the project

--- app/src/main/java/com/digidentity/codeassignment/catalog/presentation/main/MainActivity.kt
+++ app/src/main/java/com/digidentity/codeassignment/catalog/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.digidentity.codeassignment.catalog
+package com.digidentity.codeassignment.catalog.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.digidentity.codeassignment.catalog.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/digidentity/codeassignment/catalog/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.digidentity.codeassignment.catalog.utils
+
+object Constants {
+}
